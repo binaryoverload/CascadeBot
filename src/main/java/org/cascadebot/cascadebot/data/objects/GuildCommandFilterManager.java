f10766391e172efb7b8dda403a3cebbd6c4e8c23
William Oldham <wegg7250@gmail.com>
2019-08-07 16:52:18 +0100

    Filter mode is now in the manager

--- src/main/java/org/cascadebot/cascadebot/data/objects/GuildCommandFilterManager.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/GuildCommandFilterManager.java
@@ -1,10 +1,18 @@
 package org.cascadebot.cascadebot.data.objects;
 
+import lombok.Getter;
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class GuildCommandFilters {
+@Getter
+public class GuildCommandFilterManager {
 
+    private FilterMode filterMode = FilterMode.OFF;
     private final List<CommandFilter> commandFilters = new CopyOnWriteArrayList<>();
+
+    public enum FilterMode {
+        WHITELIST, BLACKLIST, OFF
+    }
 
 }
