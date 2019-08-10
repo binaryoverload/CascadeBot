ff9f8e579e4a97ab9e033cea1f31aa6fdac8c5cb
William Oldham <wegg7250@gmail.com>
2019-08-10 18:15:15 +0100

    Add proper methods to edit the root filters

--- src/main/java/org/cascadebot/cascadebot/data/objects/GuildCommandFilterManager.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/GuildCommandFilterManager.java
@@ -1,18 +1,36 @@
 package org.cascadebot.cascadebot.data.objects;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-@Getter
 public class GuildCommandFilterManager {
 
+    @Getter
+    @Setter
     private FilterMode filterMode = FilterMode.OFF;
     private final List<CommandFilter> commandFilters = new CopyOnWriteArrayList<>();
 
     public enum FilterMode {
         WHITELIST, BLACKLIST, OFF
+    }
+
+    public List<CommandFilter> getCommandFilters() {
+        return List.copyOf(commandFilters);
+    }
+
+    public void addFilter(CommandFilter filter) {
+        commandFilters.add(filter);
+    }
+
+    public boolean removeFilter(CommandFilter filter) {
+        return commandFilters.remove(filter);
+    }
+
+    public CommandFilter removeFilter(int index) {
+        return commandFilters.remove(index);
     }
 
 }
